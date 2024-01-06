f85336788523cd3010f5bf084e9238aa2eac6367
miiinnjiii <gyaese@naver.com>
2024-01-06 17:36:08 +0900

    ver 202401061736

---
+++ loop.py
@@ -0,0 +1,22 @@
+# 반복문
+
+for i in range(5): # 0 ~ 4번째 반복
+    print("Hi~") 
+    
+number = 0
+while number < 10 : # 숫자가 10보다 작은 경우에만 반복함
+    print(number)
+    number = number + 1
+    
+    
+    
+# 조건문 
+name = "Min"
+
+if name == "Min":
+    print("YOUR NAME IS MIN")
+    
+else:
+    print("YOUR NAME IS NOT MIN")
+    
+
