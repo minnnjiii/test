a243d3ebd86d50e926046a36bb02718bcebbd180
miiinnjiii <gyaese@naver.com>
2024-01-06 17:23:43 +0900

    ver202401061723

---
+++ problem1.py
@@ -0,0 +1,24 @@
+# 입력 받고 출력 하기
+
+
+
+# case 1  : 입력이 단순히 정수일 때, 
+number = int(input())
+
+# case 2 : 수열 
+First,second,Third = map(int,input().split())
+
+print(First, second, Third )
+
+# case 3 : 단순히 문자 일 때 
+string = input()
+
+# case 4 : 문자열
+
+First, Second, Third = map(str,input().split())
+print(First, second, Third )
+
+
+print("입력하신 숫자 : ", number)
+print(number," + ",number," : ",number+number)
+
