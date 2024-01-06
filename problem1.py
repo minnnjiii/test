f8102d8dc8bfc43ee2fe07a87ef9df232945730b
miiinnjiii <gyaese@naver.com>
2024-01-06 17:32:09 +0900

    ver202401061732

--- problem1.py
+++ problem1.py
@@ -1,24 +1,19 @@
 # 입력 받고 출력 하기
 
+# 숫자를 입력 받을 때는 int(input())
+# 문자를 입력 받을 때는 input()
+
+# 여러가지 숫자들의 조합 = 수열
+# 여러가지 문자들의 조합 = 문자열
+
+# list(map(int,input().split()))
+# list(map(str,input().split()))
+
+listMin = list(map(int,input().split()))
+listM = list(map(str,input().split()))
+
+print(*listMin)
+print(*listM)
 
 
-# case 1  : 입력이 단순히 정수일 때, 
-number = int(input())
-
-# case 2 : 수열 
-First,second,Third = map(int,input().split())
-
-print(First, second, Third )
-
-# case 3 : 단순히 문자 일 때 
-string = input()
-
-# case 4 : 문자열
-
-First, Second, Third = map(str,input().split())
-print(First, second, Third )
-
-
-print("입력하신 숫자 : ", number)
-print(number," + ",number," : ",number+number)
 
