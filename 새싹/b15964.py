45f53cb0c1f02af4e924b0c86cec5376a3e86192
miiinnjiii <gyaese@naver.com>
2024-01-07 21:23:23 +0900

    ver202401072123

---
+++ 새싹/b15964.py
@@ -0,0 +1,5 @@
+# 이상한 기호
+
+A,B = map(int,input().split())
+sum = (A+B)*(A-B)
+print(sum)
---
+++ 새싹/b2475.py
@@ -0,0 +1,7 @@
+# 검증수 
+
+res = 0
+for n in list(map(int,input().split())):
+    res += n**2
+print(res%10)
+
