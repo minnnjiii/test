f5daa9be8820bb577d54c0a4cbcd56e6feddcd48
miiinnjiii <gyaese@naver.com>
2024-01-07 17:21:04 +0900

    ver202401071721

--- 새싹/b2738번.py
+++ 새싹/b2738번.py
@@ -2,3 +2,22 @@
 
 # N*M크기의 두 행렬 A와 B가 주어졌을 때, 두 행렬을 더하는 프로그램을 작성하시오.
 
+A, B = [], []
+N, M = map(int, input().split())
+
+for row in range(N):
+    row = list(map(int,input().split()))
+    A.append(row)
+    
+for row in range(N):
+    row = list(map(int,input().split()))
+    B.append(row)
+    
+for row in range(N):
+    for col in range(M):
+        print(A[row][col] + B[row][col], end = ' ')
+    print() # 줄바꿈 
+    
+
+    
+    
