9fc0b0da99a0159d5fc767f4e77ac0d1722f8f6f
miiinnjiii <gyaese@naver.com>
2024-01-13 16:20:28 +0900

    ver01131620

---
+++ 브론즈/b10809.py
@@ -0,0 +1,7 @@
+# 알파벳 찾기
+
+S = input()
+
+for x in 'abcdefghijklmnopqrstuvwxyz':
+    print(S.find(x),end=' ')
+    
---
+++ 브론즈/b11720.py
@@ -0,0 +1,7 @@
+# 숫자의 합
+
+num = int(input())
+nums = map(int,list(input()))
+
+sum = sum(nums)
+print(sum)
