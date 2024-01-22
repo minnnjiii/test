473dfdd977be7bdfdb7d8ff0f9c1ffc377be167d
miiinnjiii <gyaese@naver.com>
2024-01-22 16:42:22 +0900

    하하

---
+++ 브론즈/브론즈4/b5543.py
@@ -0,0 +1,11 @@
+# 상근날드
+
+burger = []
+for i in range(3):
+    burger.append(int(input()))
+    
+drink= [] 
+for i in range(2):
+    drink.append(int(input()))
+    
+print((min(burger)+min(drink)-50))
