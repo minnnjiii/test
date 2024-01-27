a050fa438eb48b5364d59282a1f0bbac32613ff7
miiinnjiii <gyaese@naver.com>
2024-01-27 23:22:02 +0900

    소인수분해

--- 브론즈/브론즈1/b11653.py
+++ 브론즈/브론즈1/b11653.py
@@ -2,6 +2,10 @@
 
 N = int(input())
 
-for i in range(1,int(N**0.5)+1):
-    if N % i == 0 :
-        print(i,N//i)
+i = 2
+while N > 1 : 
+    if N % i  == 0:
+        N = N // i 
+        print(i)
+    else:
+        i += 1
