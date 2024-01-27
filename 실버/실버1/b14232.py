e5b9e59a44a0051b1f677b288edba75846b170ee
miiinnjiii <gyaese@naver.com>
2024-01-27 23:56:56 +0900

    모르겠다..

---
+++ 실버/실버1/b14232.py
@@ -0,0 +1,14 @@
+# 보석 도둑_b14232
+
+n = int(input())
+jewel = [] 
+
+for i in range(1,int(n**0.5)+1):
+    if i >2 :
+        if n%i == 0:
+            jewel.append(i)
+            jewel.append(n//i)
+
+jewel.sort()
+print(len(jewel))
+print(*jewel)
