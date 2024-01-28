a2428a50a16cc30c63b9579901ac375c23c67cf0
miiinnjiii <gyaese@naver.com>
2024-01-28 17:31:06 +0900

    두나무

--- 브론즈/브론즈4/b11948.py
+++ 브론즈/브론즈4/b11948.py
@@ -1,2 +1,17 @@
 # 과목선택_11948
 
+# 과학 과목
+science = [] 
+
+for i in range(4):
+    science.append(int(input()))
+    
+# 사회 과목
+society = []
+
+for i in range(2):
+    society.append(int(input()))
+
+# 과학 과목 내림차순으로 정렬하기    
+science.sort(reverse=True)
+print(sum(science[0:3],max(society)))
