496b811740db8b2402b756808d8078558893e6eb
miiinnjiii <gyaese@naver.com>
2024-02-04 00:57:25 +0900

    취업 빨리 되게 해주세요

--- 브론즈/브론즈2/b10811.py
+++ 브론즈/브론즈2/b10811.py
@@ -2,6 +2,10 @@
 
 N , M = map(int,input().split())
 
-for i in range(M):
+b = [i for i in range(N+1)]
+
+for _ in range(M):
     i, j = map(int,input().split())
-    
+    b[i:j+1] = reversed(b[i:j+1])
+b.remove(b[0])
+print(*b)
