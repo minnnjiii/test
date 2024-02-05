fe8b618848ea53a40e46aaf432c01807c00f3346
miiinnjiii <gyaese@naver.com>
2024-02-05 21:44:24 +0900

    랄ㄹ랄ㄹㄹㄹㄹ

--- 브론즈/브론즈1/b10798.py
+++ 브론즈/브론즈1/b10798.py
@@ -1,3 +1,18 @@
 #세로읽기 
 
+word = []
+for i in range(5):
+    word.append(input())
+   
+big = [] 
 
+for i in range(5):
+    big.append(len(word[i])) 
+
+answer = ''
+for i in range(0,max(big)):
+    for j in range(0,5):
+       if i < len(word[j]):
+            answer += word[j][i]
+       
+print(answer)
