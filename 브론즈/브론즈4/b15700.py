cbc06498ca3ae888079a1915380d63dd671e919f
miiinnjiii <gyaese@naver.com>
2024-02-21 23:22:44 +0900

    0221

---
+++ 브론즈/브론즈4/b15700.py
@@ -0,0 +1,6 @@
+# 타일 채우기 4
+# 15700번 
+
+n,m = map(int,input().split())
+
+print((n*m)//2)
---
+++ 브론즈/브론즈4/b23795.py
@@ -0,0 +1,13 @@
+# 사장님 도박은 재미로 하셔야 합니다
+# 23795번 
+
+
+money = 0 
+
+while True:
+    M = int(input())
+    if M == -1 : 
+        break 
+    money += M 
+    
+print(money)
