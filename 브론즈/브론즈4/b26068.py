09c5b30a6201504a1dcc78e85622871d90524595
mijiii <gyaese@naver.com>
2024-03-11 18:04:50 +0900

    치킨댄스를 추는 곰곰이를 본 임스2

--- 브론즈/브론즈4/b26068.py
+++ 브론즈/브론즈4/b26068.py
@@ -1,4 +1,13 @@
 # 치킨댄스를 추는 곰곰이를 본 임스2 
 # 26068번 
 
-n = int(input())
+n = int(input()) # 선물받은 횟수
+
+count = 0 # 임스가 사용할 기프티콘 개수
+for i in range(n):
+    rest = input().replace("D-", "").strip()
+    rest = int(rest)
+    if rest <= 90 : 
+        count += 1
+
+print(count)
