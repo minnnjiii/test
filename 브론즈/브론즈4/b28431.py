965b9d2dd352395e4e33726bb6dd5b5f0059ba2d
mijiii <gyaese@naver.com>
2024-03-13 15:45:12 +0900

    양말

--- 브론즈/브론즈4/b28431.py
+++ 브론즈/브론즈4/b28431.py
@@ -1,3 +1,15 @@
 # 양말 짝 맞추기
 # 28431번
 
+
+sock = [] 
+
+for i in range(5):
+    num = int(input())
+
+    if num in sock:
+        sock.remove(num)
+    else:
+        sock.append(num)
+
+print(sock[0])
