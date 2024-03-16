4f5b404be0c5e2f509d1eed52442914fbd0852b2
mijiii <gyaese@naver.com>
2024-03-16 16:45:15 +0900

    0316

--- 브론즈/브론즈3/b2506.py
+++ 브론즈/브론즈3/b2506.py
@@ -1,3 +1,25 @@
 # 점수계산
+# 백준 2506번
+
 
 n = int(input()) 
+ans = [0]
+
+scores = list(map(int,input().split())) 
+
+
+
+for i in range(len(scores)):
+    if i == 0 :
+        if scores[0] == 1 : 
+            ans.append(1)
+    else:
+        if scores[i] == 1 : 
+            if scores[i-1] == 1 : 
+                ans.append(ans[-1]+1) 
+            else : 
+                ans.append(1) 
+        else: 
+            ans.append(0) 
+
+print(sum(ans))
