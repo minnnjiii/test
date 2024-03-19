486993c8230a874ebc3c96eeb93674811c6fddb6
mijiii <gyaese@naver.com>
2024-03-19 23:50:17 +0900

    0319

---
+++ 브론즈/브론즈3/b10707.py
@@ -0,0 +1,10 @@
+# 수도요금
+# 10707
+
+a = int(input()) # X사의 1리터당 요금 A
+b = int(input()) # Y사의 기본요금 B
+c = int(input()) # Y사의 요금이 기본요금이 되는 사용량의 상한 C
+d = int(input()) # Y사의 1리터 당 추가요금 D
+p = int(input()) #  JOI군의 집에서 사용하는 한 달간 수도의 양 P
+
+
