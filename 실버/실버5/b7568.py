02da66ef6bd0492196a1e34e6730c1b9c6cfac88
mijiii <gyaese@naver.com>
2024-04-13 16:46:18 +0900

    0413

---
+++ 실버/실버5/b7568.py
@@ -0,0 +1,21 @@
+# 덩치
+# 백준/7568번
+# 실버5
+
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+people = []
+for _ in range(N):
+    x,y = map(int,input().split()) 
+    people.append((x,y))
+
+for i in people:
+    rank = 1
+    for j in people:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+    print(rank, end = " ")
