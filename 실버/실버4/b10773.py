822a1a1369f79e27e7bad560a20249294c5e85cf
mijiii <gyaese@naver.com>
2024-04-13 16:08:37 +0900

    0413

--- 실버/실버4/b10773.py
+++ 실버/실버4/b10773.py
@@ -1,3 +1,19 @@
 # 제로
 # 백준/10773번
 
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+stack = []
+for _ in range(N):
+    num = int(input())
+
+    if num == 0:
+        stack.pop()
+    else:
+        stack.append(num)
+
+print(sum(stack))
