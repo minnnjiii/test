82f589ecbcb3b04529a795a2ddabab9664e565cf
mijiii <gyaese@naver.com>
2024-04-14 20:48:46 +0900

    0414

---
+++ 실버/실버2/b1654.py
@@ -0,0 +1,23 @@
+# 랜선 자르기
+# 백준/1654번
+
+import sys
+input = sys.stdin.readline
+
+K, N = map(int,input().split())
+
+lan = [int(input()) for _ in range(K)]
+
+start, end = 1, max(lan) 
+
+while start <= end:
+    mid = (start + end) // 2
+    lines = 0 
+    for i in lan:
+        lines += i // mid 
+
+    if lines >= N : 
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
