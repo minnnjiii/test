7c2fd922827e8f41f513694150b860264c1b286e
mijiii <gyaese@naver.com>
2024-04-21 02:21:51 +0900

    0421

--- 실버/실버5/b1676.py
+++ 실버/실버5/b1676.py
@@ -1,6 +1,17 @@
 # 팩토리얼 0의 개수
 # 1676번 
 
+import math
 import sys
 input = sys.stdin.readline
 
+N = int(input())
+n = str(math.factorial(N))
+count = 0
+for i in range(len(n)-1,0,-1):
+    if n[i] != '0' : 
+        break 
+    elif n[i] == '0' : 
+        count += 1 
+
+print(count)
