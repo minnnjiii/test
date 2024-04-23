ea376330045f8a72739b243e69907769d21f5947
mijiii <gyaese@naver.com>
2024-04-24 02:43:06 +0900

    0424

--- 실버/실버5/b11866.py
+++ 실버/실버5/b11866.py
@@ -1,3 +1,27 @@
 # 요세푸스 문제 0
 # 백준/11866번
 
+import sys
+input = sys.stdin.readline
+
+
+
+N,K = map(int,input().split())
+
+'''
+1,2,3,4,5,6,7 
+6 3 1 7 5 2 4
+'''
+
+
+yo = [i for i in range(1,N+1)]
+que = []
+
+idx = 0
+while yo:
+    idx += K - 1
+    if idx >= len(yo):
+        idx %= len(yo)
+    que.append(str(yo.pop(idx)))
+
+print("<",", ".join(que), ">",sep="")
