a5dd14658319cec099476c13094090cccbd2d87d
mijiii <gyaese@naver.com>
2024-04-24 01:56:18 +0900

    0424

--- 브론즈/브론즈1/b11050.py
+++ 브론즈/브론즈1/b11050.py
@@ -1,3 +1,12 @@
+# 이항 계수 1
+# 백준/11050번
+
+import math
 import sys
 input = sys.stdin.readline
 
+def bino_factorial(n,k):
+    return math.factorial(n) // (math.factorial(n-k)*math.factorial(k))
+
+N,K = map(int,input().split())
+print(bino_factorial(N,K))
