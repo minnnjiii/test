745d2e1e209a916bd84df9c21f7499b42d4e3bdc
mijiii <gyaese@naver.com>
2024-04-30 23:57:25 +0900

    0430

--- 골드/골드4/b19942.py
+++ 골드/골드4/b19942.py
@@ -5,42 +5,45 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(99999999)
 
+def recur(idx,protein,b,c,d,price):
+    global ans
+    global used
+    global ans_used
+
+    if protein >= mp and b >= mf and c >= ms and d >= mv  : 
+        if ans > price:
+            ans = min(ans,price)
+            ans_used = []
+            for i in used:
+                ans_used.append(i)
+
+
+    if idx == N:
+        return
+    
+    used.append(idx+1)
+    recur(idx+1,nut[idx][0]+protein,nut[idx][1]+b,nut[idx][2]+c,nut[idx][3]+d,nut[idx][4]+price)
+ 
+    used.pop()
+    recur(idx+1, protein, b, c, d, price)
+
+
 
 
 N = int(input()) 
 mp, mf, ms, mv = map(int,input().split())
 nut = [list(map(int,input().split())) for _ in range(N)]
 
-def recur(idx,a,b,c,d,price):
-    global ans
-    global res
-    global cnt
-
-    if idx == N:
-        if a > mp and b > mf and c > ms and d > mv  : 
-            ans = min(ans,price)
-            res.append(price)
-        return
-
-    recur(idx+1,nut[idx][0]+a,nut[idx][1]+b,nut[idx][2]+c,nut[idx][3]+d,nut[idx][4]+price)
-    recur(idx+1, nut[idx][0], nut[idx][1], nut[idx][2], nut[idx][3], nut[idx][4])
-
-
-def _recur(_idx):
-
-    if _idx == N:
-        # 영양소 다 추가한조건 추가 
-        print("success")
-        return
-    
-    for i in range(N):
-        return
-   
-
-res = []
 ans = 1e9
-cnt = 0
+ans_used = []
+used = []
 
 recur(0,0,0,0,0,0)
-print(min(res))
-print(ans)
+
+ans_used.sort()
+if ans:
+    print(ans)
+    print(*ans_used)
+
+else:
+    print(-1)
