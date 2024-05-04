0e0ddbd950b6fd5eb4ddeca6af364bbaacc8eca2
mijiii <gyaese@naver.com>
2024-05-04 23:53:05 +0900

    0504 소인수분해

--- ✅swea/D2/s1945.py
+++ ✅swea/D2/s1945.py
@@ -6,5 +6,22 @@ num = []
 for _ in range(T):
     num.append(int(input())) 
 
-for i in range(T):
-    num[i] 
+for i in range(T): 
+    j = 2 
+    number = num[i] 
+    ans = []
+    while number > 1:
+        if number % j == 0 : 
+            number /= j 
+            ans.append(j)
+        else:
+            j += 1 
+
+    answer = []
+    for k in [2,3,5,7,11]: 
+        sosu = ans.count(k)
+        if sosu:
+            answer.append(sosu)
+        else:
+            answer.append(0)
+    print("#{}".format(i+1),*answer)
