48eae9c97f9fab18204739c1599aba24bbd4cf5c
mijiii <gyaese@naver.com>
2024-05-09 18:13:38 +0900

    0509

--- 백준/실버/실버3/b3273.py
+++ 백준/실버/실버3/b3273.py
@@ -6,4 +6,13 @@ input = sys.stdin.readline
 
 
 n = int(input()) 
+arr = list(map(int,input().split())) 
+ans = int(input())
 
+arr.sort()
+cnt = 0
+while True:
+    for i in range(n):
+        if arr[i] + arr[-1] == ans:
+            cnt += 1 
+            
