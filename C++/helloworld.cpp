000be648bc1c74157ee0a0e040a08d07f8fe4f04
mijiii <gyaese@naver.com>
2024-07-12 22:25:39 +0900

    0712

--- C++/helloworld.cpp
+++ C++/helloworld.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-
-    cout << "hello world\n";
+    cout << "Hello World!";
     return 0;
-
 }
---
+++ C++/다시c10699.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <ctime>
+
+int main(){
+
+    time_t timer = time(NULL);
+    struct tm* t = localtime(&timer);
+
+    printf("%d-%d-%d", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday);
+    return 0;
+}
