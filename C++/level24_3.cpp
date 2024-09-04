7d3820cbb0670fe8faa683ae112bb5be2c169fa8
정민지 <gyaese@naver.com>
2024-09-05 05:46:02 +0900

    0905

---
+++ C++/level24_3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+
+    string s[100];
+    for (int i = 0; i < n; i++)
+    {
+        string word;
+        cin >> word;
+        s[i] = word;
+    }
+    
+
+    int cnt = 0;
+    
+    int idx = 0;
+    while(true)
+    {
+        idx
+    }
+
+
+}
