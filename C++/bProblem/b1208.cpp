4eec744e060e56e362fa95c30e10f644f1eed457
정민지 <gyaese@naver.com>
2024-09-14 19:18:47 +0900

    20ㄹ9ㄴㅇ

---
+++ C++/bProblem/b1208.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int N, S; 
+int arr[40] = {0,};
+
+int main()
+{
+    cin >> N >> S;
+    for (int num = 0; num < N; num++)
+    {
+        cin >> arr[num];
+    }
+    
+    
+
+
+
+
+}
