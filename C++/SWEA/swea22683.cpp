3586ff71aaa63ffabfba3fb573c3a927bf428c65
정민지 <gyaese@naver.com>
2024-10-06 19:57:59 +0900

    241006

--- C++/SWEA/swea22683.cpp
+++ C++/SWEA/swea22683.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <queue>
+
 
 using namespace std;
 
@@ -17,6 +19,15 @@ using namespace std;
 
 int T; //test case
 int N, K; //맵의 크기 N, 나무를 벨 수 있는 횟수 K
+int init_X, init_Y; // RC카의 처음 시작 위치
+int target_X, target_Y; // 목표지점
+char visited[10][10] = {0,};
+
+struct Position
+{
+    int x,y;
+};
+
 int main()
 {
     cin >> T; //test case
@@ -24,17 +35,41 @@ int main()
     {
         cin >> N >> K; //맵의 크기 N, 나무를 벨 수 있는 횟수 K
         vector<vector<char>> map(N, vector<char>(N)); // 맵 초기화
+        
+        // ---------- 맵 정보 입력 받기 ------------
         for (int i = 0; i < N; i++)
         {
             for (int j = 0; j < N; j++)
             {
                 scanf(" %c", &map[i][j]); // 앞에 공백 추가
+                if (map[i][j] == 'X') 
+                {   // RC카의 처음 시작 위치 좌표 받기
+                    init_X = i;
+                    init_Y = j;
+                }
+                else if (map[i][j] == 'Y') 
+                {   // 목표지점 좌표 받기
+                    target_X = i;
+                    target_Y = j;
+                }
+
             }
-            
+        }
+        // ---------- 맵 정보 입력 받기 ------------
+        
+        // BFS 
+        
+        queue<Position> q;
+        visited[init_X][init_Y] = 1;
+        q.push({init_X, init_Y});
+
+        while (!q.empty())
+        {
+            Position now = q.front();
+            q.pop();
+
         }
         
-        
-
 
         cout << '#' << testcase + 1 << ' ' << '\n';
     }
